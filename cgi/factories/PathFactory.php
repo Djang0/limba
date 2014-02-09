eda69291ac8daaeecd4d8e6346673b6cd352c4e1
Ludovicreenaers <Ludovicreenaers@192.168.1.4>
2014-02-09 17:04:13 +0100

    config path acquisition

--- cgi/factories/PathFactory.php
+++ cgi/factories/PathFactory.php
@@ -9,25 +9,26 @@
 class PathFactory{
 	public static function getConfigPath(){
 		
-	//	$srvtab = explode("/",$_SERVER['DOCUMENT_ROOT']);
-	//	$cpt= count($srvtab);
-		//if($srvtab[$cpt-1]==''){
-			//unset($srvtab[$cpt-1]);
-			//$cpt= count($srvtab);
-		//}
-		//if($_SERVER['DOCUMENT_ROOT'][1]==':'){
-			//$xmlfile='';
-		//}else{
-			//$xmlfile='/';
-		//}
-		//foreach ($srvtab as $itm){
-			//if($itm <>'' && $itm<>$srvtab[$cpt-1]){
-				//$xmlfile = $xmlfile.$itm.'/';
-			//}elseif ($itm==$srvtab[$cpt-1]){
-				//$xmlfile = $xmlfile.'config/';
-		//	}
-		//}
-		return $_SERVER["conf_path"];
+		$srvtab = explode("/",$_SERVER['DOCUMENT_ROOT']);
+		$cpt= count($srvtab);
+		if($srvtab[$cpt-1]==''){
+			unset($srvtab[$cpt-1]);
+			$cpt= count($srvtab);
+		}
+		if($_SERVER['DOCUMENT_ROOT'][1]==':'){
+			$xmlfile='';
+		}else{
+			$xmlfile='/';
+		}
+		foreach ($srvtab as $itm){
+			if($itm <>'' && $itm<>$srvtab[$cpt-1]){
+				$xmlfile = $xmlfile.$itm.'/';
+			}elseif ($itm==$srvtab[$cpt-1]){
+				$xmlfile = $xmlfile.'config/';
+			}
+		}
+		return $xmlfile;
+		//return $_SERVER["conf_path"];
 	}
 
 }
